09ad548f4899a3548bdd970d7690363627ed52a3
josieek <kummer.j@northeastern.edu>
2023-07-17 18:09:41 -0400

    wider window

--- Simulator.py
+++ Simulator.py
@@ -2,7 +2,7 @@ import pygame
 import math
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 900, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
